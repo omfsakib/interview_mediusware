e942284c5051a6e55238600b44231ae255e45ff8
Ahmed Sakib <55790263+omfsakib@users.noreply.github.com>
2023-01-03 01:04:03 +0600

    project setup main branch

---
+++ django-coding-test/src/templates/assets/js/main.js
@@ -0,0 +1,18 @@
+// window.$ = window.jQuery = require('jquery');
+// import 'startbootstrap-sb-admin-2/js/sb-admin-2'
+// import Vue from 'vue';
+
+// import '../scss/main.scss'
+
+// window.Vue = Vue
+
+// Vue.component('create-product', require('./components/product/CreateProduct.vue').default)
+
+// const main = new Vue({
+//     el: '#app'
+// })
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
